e115bd7e2ec31f1caaeea57bd2874ba5df21917d
Ana Beatriz <caldas.ana9@gmail.com>
2023-10-30 16:44:05 -0300

    feat: início da lógica de login

---
+++ backend/src/services/authService.js
@@ -0,0 +1,13 @@
+const { compareSync } = require("bcrypt");
+const knex = require("../database/");
+const jwt = require("jsonwebtoken");
+const yup = require("yup");
+
+module.exports = {
+    async login(email, password){
+        userValidation = yup.object({
+            email: yup.string().email("O email inserido não está em um formato válido."),
+            password: yup.string()
+        })
+    }
+}
