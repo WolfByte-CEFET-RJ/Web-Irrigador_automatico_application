068e22d37bf0dd801296ce5680074c48e3ab3e1a
Ana Beatriz <caldas.ana9@gmail.com>
2023-10-31 00:41:11 -0300

    feat: criação da rota de login e continuação da lógica

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('./controllers/userController');
+const authController = require('./controllers/authController');
 
 router
-      .get('/', async (req, res) => {
-        res.send('Hello World!')
-      })
       .get('/user/:id?', userController.getUsers)
+
       .post('/user', userController.createUser)
+      .post('/login', authController.login)
+
       .patch('/user/:id', userController.updateUser)
-      .delete('/user/:id', userController.deleteUser);
+
+      .delete('/user/:id', userController.deleteUser)
       
 
 module.exports = router;
--- backend/src/services/authService.js
+++ backend/src/services/authService.js
@@ -6,8 +6,20 @@ const yup = require("yup");
 module.exports = {
     async login(email, password){
         const userValidation = yup.object({
-            email: yup.string().email("O email inserido não está em um formato válido."),
-            password: yup.string()
+            email: yup.string().email("O email inserido não está em um formato válido.").required(),
+            password: yup.string().required()
         });
+
+        await userValidation.validate({email, password});
+
+        const user = (await knex("user").select("email", "password").where({email}).first());
+
+        if (!user){
+            throw new Error("O email informado não foi cadastrado!");
+        }
+
+        if (!(compareSync(password, user.password))){
+            throw new Error("Senha incorreta!");
+        }
     }
 }
