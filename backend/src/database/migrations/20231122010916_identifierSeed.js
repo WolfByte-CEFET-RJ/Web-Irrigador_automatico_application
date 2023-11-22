c928864b735fb8b573be790b79cd1d3189c2b3c7
Matheus de Souza Figueiredo <matheus.szfig@gmail.com>
2023-11-21 22:35:39 -0300

    gen-seed

---
+++ backend/src/database/migrations/20231122010916_identifierSeed.js
@@ -0,0 +1,20 @@
+const GenSeed = require("../../utils/identifierSeed");
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return [
+    knex('identifier').insert({value:GenSeed(1)}),
+    knex('identifier').insert({value:GenSeed(2)}),
+  ]
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex('identifier').truncate()
+};
