144d662648aeaf9b6b5a8f5aeb735749060b1844
Isabele Rocha <isabeledpr@gmail.com>
2023-12-10 16:16:23 -0300

    feat: finalização login

--- frontend/src/routes/routes.js
+++ frontend/src/routes/routes.js
@@ -9,7 +9,7 @@ const Stack = createStackNavigator();
 const Navigation = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home" screenOptions={{headerShown: false}}>
+      <Stack.Navigator initialRouteName="SignIn" screenOptions={{headerShown: false}}>
         <Stack.Screen name="SignUp" component={SignUp} />
         <Stack.Screen name="SignIn" component={LogIn} />
         <Stack.Screen name="Home" component={Home} />
