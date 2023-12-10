c7283577adaeaa38f03cbdf0b9fbcf804da5c4e4
Gabriel Costa <71232298+gabrielcostaall@users.noreply.github.com>
2023-12-10 13:26:46 -0300

    feat: crud(irrigationSetting)
    Implementação do CRUD da entidade Irrigation Settings sem a implementação das regras de negócio

--- backend/src/controllers/irrigationSettingController.js
+++ backend/src/controllers/irrigationSettingController.js
@@ -13,7 +13,51 @@ module.exports = {
         const setting = await irrigationSettingService.getOneSetting(id);
         return res.status(200).json(setting);
 
-    } catch (error) {
+        } catch (error) {
         return res.status(400).json({ message: error.message });
+        }   
+    },
+    async createIrrigationSetting(req, res) {
+        const { name } = req.body;
+        const userId = req.user_id;
+
+        try {
+            const response = await irrigationSettingService.createIrrigationSetting(name, userId);
+            return res.status(201).json({ message: response });
+
+        } catch (error) {
+            return res.status(400).json({ message: error.message });
+        }
+    },
+    async updateIrrigationSetting(req, res) {
+        const {id} = req.params; 
+        const myId = req.user_id
+
+        const settingData = req.body;
+        try {
+            const setting = await irrigationSettingService.updateIrrigationSetting(myId, id, settingData);
+            if (setting) {
+                res.json({ message: setting });
+            }
+
+        } catch (error) {
+            return res.status(400).json({ message: error.message });
+        }
+    },
+    async deleteIrrigationSetting(req, res) {
+        const userId = req.user_id;
+        const {settingId} = req.params;
+
+        try {
+          
+            const setting = await irrigationSettingService.deleteIrrigationSetting(settingId, userId);
+            if(setting){
+                res.json({message: 'Config deletada com sucesso!'})
+            }
+
+        } catch (error) {
+            return res.status(400).json({message: error.message});
+        }
     }
-}}
+
+}
--- backend/src/services/irrigationSettingService.js
+++ backend/src/services/irrigationSettingService.js
@@ -9,6 +9,41 @@ module.exports = {
         if (!setting){throw new Error('Esta config não existe')};
 
         return setting;
-    }
+    },
+    async createIrrigationSetting(name, userId) {
+        
+
+
+        await knex('irrigationSetting').insert({
+            name,
+            userId
+        });
+
+
+        return "Configuração cadastrada!"
+    },
+    async updateIrrigationSetting(myId, id, settingData) {
+        
+        const {userId} = await this.getOneSetting(id);
+
+
+        if (myId != userId){throw new Error("Você só pode atualizar sua própria config")}
+        if(settingData.userId){throw new Error('Você não pode alterar o userId')}
+
+
+        const setting = await knex('irrigationSetting').where({ id }).update(settingData);
+        if (!setting){throw new Error('Esta configuração não existe')}
+
+        return "Configuração atualizada com sucesso!"
+    },
+    async deleteIrrigationSetting(settingId, userId) {
+        
+        const setting = await this.getOneSetting(settingId); 
+        if (!setting){
+            throw new Error('Esta config não existe!')
+        }
+        return knex('irrigationSetting').where({ id: settingId }).del();
+      
+      }
 
 };
