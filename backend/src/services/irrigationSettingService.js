dd60b9a4ba9ebf0903992757aef6e7b74bbd8904
Ana Beatriz <caldas.ana9@gmail.com>
2023-12-15 19:46:54 -0300

    feat: valores da irrigation adicionados no update, delete e inicio no read

--- backend/src/services/irrigationSettingService.js
+++ backend/src/services/irrigationSettingService.js
@@ -5,8 +5,17 @@ module.exports = {
         return await knex('irrigationSetting').select('*');
     },
     async getOneSetting(id) {
-        const setting = await knex('irrigationSetting').where({ id }).first();
+        //const setting = await knex('irrigationSetting').select('*').join('configSensor', 'irrigationSetting.id', '=', 'configSensor.irrigationId').where({ id });
+        const setting = await knex('irrigationSetting').select('*').where({ id }).first();
         if (!setting){throw new Error('Esta config não existe')};
+
+        //const finalSetting = Object.assign(setting[0], setting[1])
+
+        const humidity = await knex('configSensor').select('value').where({irrigationId: setting.id, sensorId: 1}).first();
+        const water = await knex('configSensor').select('value').where({irrigationId: setting.id, sensorId: 2}).first();
+
+        setting.humidityValue = humidity.value
+        setting.waterValue = water.value
 
         return setting;
     },
@@ -29,16 +38,26 @@ module.exports = {
     },
     async updateIrrigationSetting(myId, id, settingData) {
         
-        const {userId} = await this.getOneSetting(id);
+        const settingInfo = await this.getOneSetting(id);
 
-
-        if (myId != userId){throw new Error("Você só pode atualizar sua própria config")}
+        if (myId != settingInfo.userId){throw new Error("Você só pode atualizar sua própria config")}
         if(settingData.userId){throw new Error('Você não pode alterar o userId')}
 
+        if (settingData.name){
+            const settingForChange = await knex('irrigationSetting').where({ id }).update({name: settingData.name});
+            if (!settingForChange){throw new Error('Esta configuração não existe')}
+        }
+       
+        if (settingData.humidityValue){
+            const humidityValue = await knex('configSensor').where({irrigationId: settingInfo.id, sensorId: 1}).update({value: settingData.humidityValue})
+            if (!humidityValue){throw new Error('Erro ao alterar o valor da umidade!')}
+        }
 
-        const setting = await knex('irrigationSetting').where({ id }).update(settingData);
-        if (!setting){throw new Error('Esta configuração não existe')}
-
+        if (settingData.waterValue){
+            const waterValue = await knex('configSensor').where({irrigationId: settingInfo.id, sensorId: 2}).update({value: settingData.waterValue})
+            if (!waterValue){throw new Error('Erro ao alterar o valor do nível da água!')}
+        }
+        
         return "Configuração atualizada com sucesso!"
     },
     async deleteIrrigationSetting(settingId, userId) {
@@ -47,7 +66,16 @@ module.exports = {
         if (!setting){
             throw new Error('Esta config não existe!')
         }
-        return knex('irrigationSetting').where({ id: settingId }).del();
+
+        const deleteValues = await knex('configSensor').where({irrigationId: settingId}).del();
+
+        if (!deleteValues){throw new Error('Erro ao apagar valores da configuração!')}
+
+        const deleteName = await knex('irrigationSetting').where({ id: settingId }).del();
+
+        if (!deleteName){throw new Error('Erro ao apagar nome da configuração!')}
+
+        return 'Configuração apagada com sucesso!'
       
       }
 
