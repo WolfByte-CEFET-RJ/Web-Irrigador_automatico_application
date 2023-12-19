fddd62231bb67a1185fbdb229ea296e000dda67e
Rhyan Pinto <rhyanpinto@gmail.com>
2023-12-19 16:28:58 -0300

    feat: irrigation warning message

--- backend/src/controllers/mqttController.js
+++ backend/src/controllers/mqttController.js
@@ -1,71 +1,34 @@
 require('dotenv').config();
-const mqtt = require('mqtt')
+
 const mqttService = require('../services/mqttService.js')
-//const client = mqtt.connect("mqtt://localhost") 
+const client = mqttService.createMqttClient(); 
 
-const { connectOptions } = require('../use_mqtts.js')
-const clientId = 'teste_nodejs_' + Math.random().toString(16).substring(2, 8)
-const options = {
-  clientId,
-  clean: true,
-  connectTimeout: 4000,
-  username: 'backend',
-  password: 'backend123',
-  reconnectPeriod: 1000,
-  rejectUnauthorized: true,
-}
-
-const {protocol, host, port} = connectOptions
-
-let connectUrl = `${protocol}://${host}:${port}`
-
-const client = mqtt.connect(connectUrl, options)
-
-const topicName = 'test/sensor'
-const payload = '{"message": "Hello World"}'
+const topicName = 'Horta/Medida'
 const qos = 0
 
 // connect to same client and subscribe to same topic name  
 client.on('connect', () => { 
-  console.log(`${protocol}: Connected`)
+  console.log(`${client.options.protocol}: Connected`)
   client.subscribe(topicName, {qos}, (err, granted) => { 
       if(err) { 
           console.log(err, 'err'); 
       } 
       console.log(granted, 'granted') 
-
-      client.publish(topicName, payload, { qos }, (error) => {
-        if (error) {
-          console.error(error)
-        }
-      })
-  }) 
-}) 
-
+  });
+});
 
 // on receive message event
 client.on('message', async (topic, message, packet) => { 
-  //console.log(packet, packet.payload.toString()); 
   console.log("Mensagem recebiba!")
   if(topic === topicName) { 
     try {
       const jsonMessage = JSON.parse(message);
-      console.log('Conteudo:', jsonMessage)
-
-      //await mqttService.insertData(jsonMessage);
-
-      /*
-        Estruturada considerada: 
-
-        "identificador": "12312345675",
-        "sensor": "Umidade",
-        "medida": "10"
-      */
-
-      
+      console.log('Conteudo:', jsonMessage);
+      await mqttService.insertData(jsonMessage);
+      await mqttService.checkAndSendIrrigationMessage(jsonMessage);
     } catch (error) {
       console.error('Erro: ', error);
       console.log('Conteúdo da mensagem:', message.toString());
     }
   } 
-}) 
+});
--- backend/src/services/mqttService.js
+++ backend/src/services/mqttService.js
@@ -1,18 +1,87 @@
 const knex = require('../database');
+const mqtt = require('mqtt');
+const { connectOptions } = require('../use_mqtts.js');
+
+function createMqttClient() {
+    const clientId = 'teste_nodejs_' + Math.random().toString(16).substring(2, 8);
+    const options = {
+        clientId,
+        clean: true,
+        connectTimeout: 4000,
+        username: 'backend',
+        password: 'backend123',
+        reconnectPeriod: 1000,
+        rejectUnauthorized: true,
+    };
+
+    const { protocol, host, port } = connectOptions;
+    const connectUrl = `${protocol}://${host}:${port}`;
+
+    return mqtt.connect(connectUrl, options);
+}
 
 module.exports = {
     async insertData(data){
-        const garden = await knex('garden').select("id").where({identifier: data.identificador});
+
+        const { identificador, sensor, medida } = data;
+
+        const garden = await knex('garden').select("id").where({identifier: identificador}).first();
 
         if (!garden){throw new Error('O identificador informado não pertence a uma horta!');}
 
-        let sensorId = data.sensor === "Umidade" ? 1 : 2;
-
+        let sensorId = sensor === "Umidade" ? 1 : 2;
+        
         await knex('measurement').insert({
-            measurement: data.medida,
+            measurement: medida,
             date: knex.fn.now(),
             sensorId: sensorId,
             gardenId: garden.id
         });
-    }
-}
+    },
+
+    async checkAndSendIrrigationMessage(data){
+
+        const { identificador, sensor, medida } = data;
+        console.log(medida)
+        const garden = await knex('garden')
+            .select('id', 'configId')
+            .where({identifier: identificador})
+            .first();
+
+        if (!garden) {
+            throw new Error('O identificador informado não pertence a uma horta!');
+        }
+
+        const configSensor = await knex('configSensor')
+            .select('value')
+            .where({
+                sensorId: knex('sensor').select('id').where({name: sensor}),
+                irrigationId: garden.configId
+            })
+            .first();
+        
+        if (!configSensor){
+            throw new Error('A configuração do sensor não foi encontrada!');
+        }
+
+        if (parseFloat(medida) < parseFloat(configSensor.value)) {
+            // Enviar mensagem para o mesmo tópico indicando que a planta deve ser irrigada
+            const client = createMqttClient();
+
+            const topicName = 'Outros/Estado_Motor';
+            const payload = `{"identificador": "${identificador}", "mensagem": "Planta precisa ser irrigada!"}`;
+            const qos = 0;
+
+            client.on('connect', () => {
+                console.log(`${client.options.protocol}: Connected`);
+                client.publish(topicName, payload, { qos }, (error) => {
+                    if (error) {
+                        console.error(error);
+                    }
+                });
+            });
+        }
+    },
+
+    createMqttClient,
+};
