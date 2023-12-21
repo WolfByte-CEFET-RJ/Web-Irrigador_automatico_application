5331210786f26668837acc7c7aeb3bf4a509c7c0
Ana Beatriz <caldas.ana9@gmail.com>
2023-12-20 22:19:43 -0300

    feat(irrigation setting): inicio da mudança para nome ser unico

--- backend/src/database/migrations/20231221003101_create_irrigationSetting.js
+++ backend/src/database/migrations/20231221003101_create_irrigationSetting.js
@@ -1,12 +1,13 @@
 
-exports.up = function(knex) {   
+exports.up = function(knex) {
     return knex.schema.createTable("irrigationSetting", (table) => {
         table.increments("id").primary();
         table.string("name").notNullable();
+        table.unique(["id", "name"]);
         table.integer("userId").unsigned().references("id").inTable("user").onDelete("CASCADE").nullable();
     })
 };
 
 exports.down = function(knex) {
-    return knex.schema.dropTable("irrigationSetting");
+    
 };
