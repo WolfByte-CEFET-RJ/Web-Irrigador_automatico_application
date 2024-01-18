a21399251ded9fe20a1e7329e92647d204f617dc
Cleytonpsf <cleytonpsf@gmail.com>
2024-01-18 01:15:23 -0300

    refactor: alteração do identifier para string

--- backend/src/database/seeds/04_identifier_seed.js
+++ backend/src/database/seeds/04_identifier_seed.js
@@ -2,7 +2,7 @@ exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('identifier').del()
   await knex('identifier').insert([
-    {value: 2024201112},
-    {value: 2024201668}
+    {value: "2024201112"},
+    {value: "2024201668"}
   ]);
 };
