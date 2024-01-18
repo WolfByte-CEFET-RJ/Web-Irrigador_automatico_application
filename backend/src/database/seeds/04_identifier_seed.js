c14f407498bd35f4e158337056de53c2ba97b9dd
Cleytonpsf <cleytonpsf@gmail.com>
2024-01-17 22:52:30 -0300

    refactor: alteração dos valores do identificador para um valor fixo

--- backend/src/database/seeds/04_identifier_seed.js
+++ backend/src/database/seeds/04_identifier_seed.js
@@ -1,10 +1,8 @@
-const GenSeed = require("../../utils/identifierSeed");
-
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('identifier').del()
   await knex('identifier').insert([
-    {value: GenSeed(1)},
-    {value: GenSeed(2)}
+    {value: 2024201112},
+    {value: 2024201668}
   ]);
 };
