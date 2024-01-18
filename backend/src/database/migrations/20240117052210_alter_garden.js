aabdb2a7bb2890e0bf61d7861c150d58c27a0ee5
Cleytonpsf <cleytonpsf@gmail.com>
2024-01-18 01:47:35 -0300

    fix: alteração da ordem de criação e remoção dos atributos
    estava dando um bug que não entendi muito bem, então alterei a ordem e corrigiu

--- backend/src/database/migrations/20240117052210_alter_garden.js
+++ backend/src/database/migrations/20240117052210_alter_garden.js
@@ -1,16 +1,16 @@
 
 exports.up = function(knex) {
     return knex.schema.alterTable("garden", async (table) => {
-        table.dropForeign("configId");
-        await table.dropColumn("configId");
         table.integer("irrigationId").unsigned().references("id").inTable("irrigationSetting").onDelete("CASCADE");
+        table.dropForeign("configId");
+        table.dropColumn("configId");
     })
 };
 
 exports.down = function(knex) {
     return knex.schema.alterTable("garden", async (table) => {
-        table.dropForeign("irrigationId");
-        await table.dropColumn("irrigationId");
         table.integer("configId").unsigned().references("id").inTable("irrigationSetting").onDelete("CASCADE");
+        table.dropForeign("irrigationId");
+        table.dropColumn("irrigationId");
     })
 };
