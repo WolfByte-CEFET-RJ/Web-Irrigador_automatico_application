994fea8ce4cf31815febd385ffa186de52ed8da1
Gabriel Costa <71232298+gabrielcostaall@users.noreply.github.com>
2024-01-29 19:07:30 -0300

    fix(irrigationSetting): Ao buscar por uma setting específica, a API irá verificar se aquela setting pertence ao usuário que está requisitando
    Além da correção na entidade "IrrigationSetting", as migrations da aplicação foram revisadas com a adição da cláusula "onDelete()" em todas os apontamentos de chaves estrangeiras

--- backend/src/database/migrations/20231027140717_create_configSensor.js
+++ backend/src/database/migrations/20231027140717_create_configSensor.js
@@ -3,8 +3,8 @@ exports.up = function(knex) {
   return knex.schema.createTable('configSensor', (table) => {
       table.integer("sensorId").unsigned();
       table.integer("irrigationId").unsigned();
-      table.foreign("sensorId").references("id").inTable("sensor");
-      table.foreign("irrigationId").references("id").inTable("irrigationSetting");
+      table.foreign("sensorId").references("id").inTable("sensor").onDelete("CASCADE");
+      table.foreign("irrigationId").references("id").inTable("irrigationSetting").onDelete("CASCADE");
       table.string("value").notNullable();
       table.primary(["sensorId", "irrigationId"]);
 
