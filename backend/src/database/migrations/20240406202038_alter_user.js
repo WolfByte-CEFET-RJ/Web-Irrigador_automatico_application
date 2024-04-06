62cc06cb36f93cfa4818dc1b6127281c9f3c72b6
Ana-beatriz2 <caldas.ana9@gmail.com>
2024-04-06 17:39:11 -0300

    refactor: apaga ultimas migrations do user e cria unico arquivo corrigido

--- backend/src/database/migrations/20240406202038_alter_user.js
+++ backend/src/database/migrations/20240406202038_alter_user.js
@@ -1,7 +1,7 @@
 
 exports.up = function(knex) {
     return knex.schema.alterTable("user", async (table) => {
-        table.integer("code");
+        table.integer("code").defaultTo(null);
         table.timestamp("expirationDate").defaultTo(null);
     })
 };
