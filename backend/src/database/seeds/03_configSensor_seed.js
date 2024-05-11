6b4835829e1fb563fe1d9694547bc19c0836ce8d
Gabriel Costa <71232298+gabrielcostaall@users.noreply.github.com>
2024-05-11 19:51:35 -0300

    fix(seeds): remoção de registros nas seeds de sensor e configSensor

--- backend/src/database/seeds/03_configSensor_seed.js
+++ backend/src/database/seeds/03_configSensor_seed.js
@@ -3,8 +3,7 @@ exports.seed = async function(knex) {
   return knex('configSensor').del()
     .then(function () {
       return knex('configSensor').insert([
-        { sensorId: 1, irrigationId: 1, value: '50' },
-        { sensorId: 2, irrigationId: 1, value: '30' },
+        { sensorId: 1, irrigationId: 1, value: '50' } 
       ]);
     });
 };
