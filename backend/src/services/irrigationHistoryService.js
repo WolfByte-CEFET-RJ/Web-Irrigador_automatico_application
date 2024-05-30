d4a314843cdfd48e2de22cb3f7930af495cebca3
Rhyan Pinto <rhyanpinto@gmail.com>
2024-05-30 16:26:24 -0300

    Fix: garden name is required

--- backend/src/services/irrigationHistoryService.js
+++ backend/src/services/irrigationHistoryService.js
@@ -18,14 +18,22 @@ module.exports = {
 
     // Retorna o histórico de irrigação de uma horta do usuário autenticado
     async getOneGardenHistory(userId, gardenName){
-        const userGardenHistory = await knex('user as u').select('g.name as gardenName', 'ih.date as date')
+        const userGardenHistory = await knex('user as u')
+        .select('g.name as gardenName', 'ih.date as date')
         .join('garden as g', 'g.userId', '=', 'u.id')
         .join('irrigationHistory as ih', 'ih.gardenId', '=', 'g.id')
-        .where({'u.id': userId, 'g.name': gardenName}).orderBy('date', 'asc')
+        .where('u.id', userId)
+        .andWhere('g.name', gardenName)
+        .orderBy('date', 'asc');
 
-        if(!userGardenHistory.length){throw new Error("Nenhum histórico de irrigação para essa horta foi encontrado ou essa não é sua horta!")}
+        if(!userGardenHistory.length){
+            throw new Error("Nenhum histórico de irrigação para essa horta foi encontrado ou essa não é sua horta!")
+        }
 
-        const finalUserGardenHistory = userGardenHistory.map(object => ({gardenName: object.gardenName, date: convertDate(object.date)}));
+        const finalUserGardenHistory = userGardenHistory.map(object => ({
+            gardenName: object.gardenName, 
+            date: convertDate(object.date)
+        }));
 
         return finalUserGardenHistory;
     }
